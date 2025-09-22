f21ae94e169366e5019816c51d51e5050d03be7a
MHARK <mharkdavid713@gmail.com>
2025-09-22 01:01:22 +0100

    adde toogle feature

--- src/component/navbar/navbar.jsx
+++ src/component/navbar/navbar.jsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import { Link as ScrollLink } from 'react-scroll'; // Import Link from react-scroll
+import { useEffect, useState } from 'react';
+import { Link as ScrollLink } from 'react-scroll';
 import './navbar.css';
 import logo from '../../assets/logo.png';
 
 const Navbar = () => {
   const [sticky, setSticky] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -23,18 +24,56 @@ const Navbar = () => {
     };
   }, []);
 
+  // Close menu on navigation (for mobile)
+  const handleMenuClick = () => setMenuOpen(false);
+
   return (
     <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
       <img src={logo} alt="Logo" className='logo' />
-      <ul>
-        {/* Use ScrollLink for smooth scrolling */}
-        <li><ScrollLink to="Home" smooth={true} duration={500}>Home</ScrollLink></li>
-        <li><ScrollLink to="program" smooth={true} duration={500}>Program</ScrollLink></li>
-        <li><ScrollLink to="about-us" smooth={true} duration={500}>About us</ScrollLink></li>
-        <li><ScrollLink to="campus" smooth={true} duration={500}>Campus</ScrollLink></li>
-        <li><ScrollLink to="testimonials" smooth={true} duration={500}>Testimonials</ScrollLink></li>
-        {/* ScrollLink should be placed inside the button */}
-        <li><button className='btn'><ScrollLink to="contact-us" smooth={true} duration={500}>Contact us</ScrollLink></button></li>
+      <div
+        className="navbar-hamburger"
+        onClick={() => setMenuOpen((open) => !open)}
+        aria-label="Toggle navigation"
+        tabIndex={0}
+        role="button"
+      >
+        <span />
+        <span />
+        <span />
+      </div>
+      <ul className={menuOpen ? 'open' : ''}>
+        <li>
+          <ScrollLink to="Home" smooth={true} duration={500} onClick={handleMenuClick}>
+            Home
+          </ScrollLink>
+        </li>
+        <li>
+          <ScrollLink to="program" smooth={true} duration={500} onClick={handleMenuClick}>
+            Program
+          </ScrollLink>
+        </li>
+        <li>
+          <ScrollLink to="about-us" smooth={true} duration={500} onClick={handleMenuClick}>
+            About us
+          </ScrollLink>
+        </li>
+        <li>
+          <ScrollLink to="campus" smooth={true} duration={500} onClick={handleMenuClick}>
+            Campus
+          </ScrollLink>
+        </li>
+        <li>
+          <ScrollLink to="testimonials" smooth={true} duration={500} onClick={handleMenuClick}>
+            Testimonials
+          </ScrollLink>
+        </li>
+        <li>
+          <button className='btn'>
+            <ScrollLink to="contact-us" smooth={true} duration={500} onClick={handleMenuClick}>
+              Contact us
+            </ScrollLink>
+          </button>
+        </li>
       </ul>
     </nav>
   );
